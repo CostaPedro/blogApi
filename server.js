e4671ed684b83c15590dc421842e939c541da67b
CostaPedro <pedromcosta.gr@hotmail.com>
2017-07-07 09:43:21 -0400

    blog api first version

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {blogPosts} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+
+blogPosts.create('My First Blog Post','Hello this is text for my blog', 'Pedro Costa');
+blogPosts.create('My Second Blog Post','Hello this is more text for my blog', 'Joe Smith');
+
+app.get('/blogPosts', (req, res) => {
+  res.json(blogPosts.get());
+});
+
+app.post('/blogPosts', jsonParser, (req, res) => {
+  const requiredFields = ['title','content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = .create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `ShoppingList.update` with updated item.
+app.put('/blogPosts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title','content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    console.log('here');
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blogPosts item \`${req.params.id}\``);
+  blogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  });
+  res.status(204).end();
+});
+
+// when DELETE request comes in with an id in path,
+// try to delete that item from ShoppingList.
+app.delete('/blogPosts/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted blogPosts item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
