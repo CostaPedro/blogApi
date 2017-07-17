8de35ca2da39bfd34d55f094ea078bb3c1103b34
CostaPedro <pedromcosta.gr@hotmail.com>
2017-07-17 10:37:55 -0400

    update 7/17

--- server.js
+++ server.js
@@ -1,79 +1,23 @@
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const {blogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
 
-// log the http layer
+
 app.use(morgan('common'));
+app.use(express.static('public'));
+
+const blogApiRouter = require('./blogApiRouter');
 
 
-blogPosts.create('My First Blog Post','Hello this is text for my blog', 'Pedro Costa');
-blogPosts.create('My Second Blog Post','Hello this is more text for my blog', 'Joe Smith');
 
-app.get('/blog-posts', (req, res) => {
-  res.json(blogPosts.get());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/blog-posts', jsonParser, (req, res) => {
-  const requiredFields = ['title','content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
 
-  const item = blogPosts.create(req.body.title, req.body.content, req.body.author);
-  res.status(201).json(item);
-});
-
-// when PUT request comes in with updated item, ensure has
-// required fields. also ensure that item id in url path, and
-// item id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `ShoppingList.update` with updated item.
-
-app.put('/blog-posts/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title','content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  if (req.params.id !== req.body.id) {
-    console.log('here');
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating blog-posts item \`${req.params.id}\``);
-  blogPosts.update({
-    id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author
-  });
-  res.status(204).end();
-});
-
-// when DELETE request comes in with an id in path,
-// try to delete that item from ShoppingList.
-app.delete('/blog-posts/:id', (req, res) => {
-  ShoppingList.delete(req.params.id);
-  console.log(`Deleted blog-posts item \`${req.params.ID}\``);
-  res.status(204).end();
-});
+app.use('/blog-posts', blogApiRouter);
 
 
 app.listen(process.env.PORT || 8080, () => {
